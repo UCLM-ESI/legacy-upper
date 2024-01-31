67a78562fb82fbd03362df7bf49a8531c31d9b8a
David Villa Alises <david.villa@gmail.com>
2024-01-31 20:07:32 +0100

    using connect() with UDP socket

---
+++ UDP_client_connect.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3 -u
+# Copyright: See AUTHORS and COPYING
+"Usage: {0} <host> <port>"
+
+import sys
+import socket
+
+if len(sys.argv) != 3:
+    print(__doc__.format(sys.argv[0]))
+    sys.exit(1)
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_endpoint = (sys.argv[1], int(sys.argv[2]))
+sock.connect(server_endpoint)
+
+while 1:
+    data = sys.stdin.readline().strip().encode()
+    if not data:
+        break
+
+    sock.send(data)
+    msg = sock.recv(1024)
+    print("Reply is '{}'".format(msg.decode()))
+
+sock.close()
